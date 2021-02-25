f460cdb1c8268aa782f387b1b632ae59fc7161fe
Clayton Gilbert <claytongilbert@gmail.com>
2021-02-26 10:57:53 +1100

    Version 0.0.4 Added location services

--- DeputyShift/Views/ShiftViewController.swift
+++ DeputyShift/Views/ShiftViewController.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 import UIKit
+import CoreLocation
 
 class ShiftViewController: UIViewController {
     
@@ -16,7 +17,11 @@ class ShiftViewController: UIViewController {
     @IBOutlet var shiftTypeSegmented: UISegmentedControl!
     @IBOutlet var actInd: UIActivityIndicatorView!
     
+    @IBOutlet var latLabel: UILabel!
+    @IBOutlet var longLabel: UILabel!
+    
     private var httpClient = HTTPClient()
+    private var locationManager = CLLocationManager()
     
     @IBAction func didPressSave(_ sender: Any) {
         actInd.isHidden = false
@@ -27,6 +32,8 @@ class ShiftViewController: UIViewController {
         formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZ"
         
         postShift.time = formatter.string(from: shiftTime.date)
+        postShift.latitude = latLabel.text
+        postShift.longitude = longLabel.text
        
         var shiftType: ShiftType = .start
         if shiftTypeSegmented.selectedSegmentIndex == 1 {
@@ -59,7 +66,33 @@ class ShiftViewController: UIViewController {
         super.viewDidLoad()
         self.messageLabel.text = ""
         self.actInd.isHidden = true
+        self.latLabel.text = "0.00000"
+        self.longLabel.text = "0.00000"
         
+        getCurrentLocation()
     }
    
+}
+
+extension ShiftViewController : CLLocationManagerDelegate {
+    
+    func getCurrentLocation() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+            //locationManager.startUpdatingHeading()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            if let location = locations.first {
+                self.latLabel.text = String(format: "%.5f", location.coordinate.latitude)
+                self.longLabel.text = String(format: "%.5f", location.coordinate.longitude)
+            }
+        }
+    
 }
